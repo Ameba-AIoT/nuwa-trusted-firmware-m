0be61d2a7ebfbfc0be532f26706b665267e16fc5
zjian zhang <zjian_zhang@realsil.com.cn>
2026-04-27 20:33:12 +0800

    [nuwa] update sdk
    tf-m version: cb849d60d65a09ef813ee2af991628e23330a7a9
    1. [RTL8721F][tfm] Optimze boot hook code

--- platform/ext/target/realtek/common/startup_bl2.c
+++ platform/ext/target/realtek/common/startup_bl2.c
@@ -6,7 +6,22 @@
  * Startup code for RTL8721F (AmebaG2) TF-M platform
  */
 
+#include "boot_prepare.h"
 #include "tfm_hal_device_header.h"
+#include "boot_hal.h"
+#include "tfm_plat_defs.h"
+#include "fih.h"
+#include "bootutil/bootutil.h"
+#include <sysflash/sysflash.h>
+
+/* NOTE: When using tfm bl2, image 0 is secure image, image 1 is app(non-secure) image */
+#define APP_IMAGE_AREA_ID FLASH_AREA_IMAGE_PRIMARY(1)
+#define APP_IMAGE_AREA_OFFSET FLASH_AREA_1_OFFSET
+#define APP_IMAGE_AREA_SIZE FLASH_AREA_1_SIZE
+
+#define IMG_AP_LOGIC_ADDR   NS_AP_LOGIC_BASE				/* 0x04000000 */
+#define IMG_NP_LOGIC_ADDR   NS_NP_LOGIC_BASE				/* 0x02000000 */
+#define FLASH_BASE_PHY_ADDR FLASH_PHY_BASE_ADDRESS	/* 0x08000000 */
 
 /*----------------------------------------------------------------------------
   External References
@@ -41,42 +56,57 @@ void Reset_Handler(void)
     __TZ_set_STACKSEAL_S((uint32_t *)(&__STACK_SEAL));
 #endif
 
-    /* __NVIC_SetVector(SVCall_IRQn, (uint32_t)SVC_Handler); */
-    /* __NVIC_SetVector(PendSV_IRQn, (uint32_t)PendSV_Handler); */
-	SVCall_irqfunc_set(SVC_Handler);
-	PendSV_irqfunc_set(PendSV_Handler);
-
     SystemInit();                             /* CMSIS System Initialization */
     __PROGRAM_START();                        /* Enter PreMain (C library entry point) */
 }
 
 /* This is a stub to make the linker happy */
-void __Vectors(){}
-void SVC_Handler(void){}
-void PendSV_Handler(void){}
-/*
- * Reset_Handler
- *     ├─► Disable interrupts (cpsid i)
- *     ├─► Set PSP, MSPLIM, PSPLIM
- *     ├─► Set SVC/PendSV handlers
- *     ├─► Data Copy Loop (__copy_table)
- *     ├─► BSS Zero Loop (__zero_table)
- *     │
- *     └─► ___start_veneer → _mainCRTStartup
- *             ├─► Set stack pointer
- *             ├─► _stack_init()
- *             ├─► memset BSS (__bss_start__-__bss_end__)
- *             ├─► __libc_init_array() (C++ constructors)
- *             ├─► atexit() registration
- *             │
- *             └─► __main_veneer → main
- *                     ├─► tfm_hal_set_up_static_boundaries()
- *                     ├─► tfm_hal_platform_init()
- *                     ├─► tfm_plat_otp_init()
- *                     ├─► Provisioning checks
- *                     ├─► tfm_arch_config_extensions()
- *                     ├─► tfm_core_validate_boot_data()
- *                     ├─► tfm_arch_set_secure_exception_priorities()
- *                     │
- *                     └─► svc #0 → SVC_Handler → TFM Scheduler
- */
+// void __Vectors(){}
+
+int boot_platform_post_load(uint32_t image_id)
+{
+	/*NOTE: image_id with value 0 represents the APP image, 1 is secure image(tfm_s)  */
+	if (image_id != 0) {
+		return 0;
+	}
+
+	boot_prepare(
+		FLASH_BASE_PHY_ADDR,
+		APP_IMAGE_AREA_ID,
+		APP_IMAGE_AREA_OFFSET,
+		APP_IMAGE_AREA_SIZE,
+		IMG_AP_LOGIC_ADDR,
+		IMG_NP_LOGIC_ADDR
+    );
+	return 0;
+}
+
+int32_t boot_platform_post_init(void)
+{
+	return 0;
+}
+
+int boot_platform_pre_load(uint32_t image_id)
+{
+	return 0;
+}
+
+bool boot_platform_should_load_image(uint32_t image_id)
+{
+	return true;
+}
+
+int flash_device_base(uint8_t fd_id, uintptr_t *ret)
+{
+	switch (fd_id) {
+	case FLASH_DEVICE_ID :
+		*ret = FLASH_BASE_PHY_ADDR;
+		break;
+	default:
+		RTK_LOGE(NOTAG, "invalid flash ID %d; expected %d",
+				 fd_id, FLASH_DEVICE_ID);
+		*ret = -1;
+		return -1;
+	}
+	return 0;
+}
